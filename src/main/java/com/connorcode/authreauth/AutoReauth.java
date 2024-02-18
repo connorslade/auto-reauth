ada1ddd79afc0672f116ea144920b656912a0543
Connor Slade <connor@connorcode.com>
2024-02-18 12:51:45 -0500

    Init project

---
+++ src/main/java/com/connorcode/authreauth/AutoReauth.java
@@ -0,0 +1,16 @@
+package com.connorcode.authreauth;
+
+import com.mojang.logging.LogUtils;
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+import org.slf4j.Logger;
+
+public class AutoReauth implements ClientModInitializer {
+    public static Logger log = LogUtils.getLogger();
+    public static MinecraftClient mc = MinecraftClient.getInstance();
+
+    @Override
+    public void onInitializeClient() {
+        log.info("Starting auto-reauth");
+    }
+}
