586219f53970e938f53d5895e3c312a77f0daad1
Connor Slade <connor@connorcode.com>
2024-02-18 20:06:43 -0500

    Use text coloring

--- src/main/java/com/connorcode/authreauth/mixin/MultiplayerScreenMixin.java
+++ src/main/java/com/connorcode/authreauth/mixin/MultiplayerScreenMixin.java
@@ -8,6 +8,9 @@ import com.mojang.authlib.exceptions.AuthenticationException;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,7 +36,14 @@ public class MultiplayerScreenMixin {
     @Inject(at = @At("TAIL"), method = "render")
     private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         var status = authStatus.getNow(AuthUtils.AuthStatus.Unknown);
-        context.drawText(client.textRenderer, String.valueOf(status), 10, 10, 0xFFFFFF, true);
+        var color = switch (status) {
+            case Unknown -> Formatting.GRAY;
+            case Invalid, Offline -> Formatting.RED;
+            case Online -> Formatting.GREEN;
+        };
+
+        var text = Text.literal(String.valueOf(status)).fillStyle(Style.EMPTY.withColor(color));
+        context.drawText(client.textRenderer, text, 10, 10, 0xFFFFFF, true);
     }
 
     @Inject(at = @At("TAIL"), method = "tick")
