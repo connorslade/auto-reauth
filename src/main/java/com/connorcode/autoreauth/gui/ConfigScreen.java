039b19292ceb8eb207dcc00cb924eaa4da815d54
Connor Slade <connor@connorcode.com>
2025-12-27 16:52:49 -0500

    Note active and default accounts

--- src/main/java/com/connorcode/autoreauth/gui/ConfigScreen.java
+++ src/main/java/com/connorcode/autoreauth/gui/ConfigScreen.java
@@ -4,18 +4,21 @@ import com.connorcode.autoreauth.Config;
 import com.connorcode.autoreauth.Main;
 import com.connorcode.autoreauth.Reauth;
 import com.connorcode.autoreauth.auth.MicrosoftAuth;
-import net.minecraft.client.gui.Click;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.PlayerSkinDrawer;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
 import net.minecraft.client.gui.widget.ThreePartsLayoutWidget;
 import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Pair;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.concurrent.Semaphore;
 
 import static com.connorcode.autoreauth.Main.*;
@@ -23,6 +26,10 @@ import static com.connorcode.autoreauth.Main.*;
 public class ConfigScreen extends Screen {
     final ThreePartsLayoutWidget layout = new ThreePartsLayoutWidget(this, 33, 60);
     AccountListWidget accountList;
+
+    ButtonWidget switchButton;
+    ButtonWidget deleteButton;
+    ButtonWidget makeDefaultButton;
 
     Screen parent;
     Semaphore semaphore = new Semaphore(0);
@@ -41,11 +48,13 @@ public class ConfigScreen extends Screen {
         var footerTop = footer.add(DirectionalLayoutWidget.horizontal().spacing(4));
         var footerBottom = footer.add(DirectionalLayoutWidget.horizontal().spacing(4));
 
-        footerTop.add(ButtonWidget.builder(Text.of("Switch"), (button) -> {
+        this.switchButton = footerTop.add(ButtonWidget.builder(Text.of("Switch"), (button) -> {
+            var selected = this.accountList.getSelectedOrNull();
+            if (selected != null) Reauth.attemptReauth(this, selected.account);
         }).width(74).build());
-        footerTop.add(ButtonWidget.builder(Text.of("Delete"), (button) -> {
+        this.deleteButton = footerTop.add(ButtonWidget.builder(Text.of("Delete"), (button) -> {
         }).width(74).build());
-        footerTop.add(ButtonWidget.builder(Text.of("Make Default"), (button) -> {
+        this.makeDefaultButton = footerTop.add(ButtonWidget.builder(Text.of("Make Default"), (button) -> {
         }).width(74).build());
         footerTop.add(ButtonWidget.builder(Text.of("Add Account"), (button) -> MicrosoftAuth.getCode(semaphore)
                 .thenCompose(MicrosoftAuth::getAccessToken).thenCompose(access -> MicrosoftAuth.authenticate(access)
@@ -61,11 +70,13 @@ public class ConfigScreen extends Screen {
                     log.error("Error re-authenticating", e);
                     Main.client.setScreen(new ErrorScreen(this, "Error re-authenticating", e.toString()));
                     return null;
-                })).width(74).build());
+                })).width(74).tooltip(Tooltip.of(Text.of("Warning: Tokens are stored in your home folder."))).build());
 
-        footerBottom.add(ButtonWidget.builder(Text.of("Debug Mode"), (button) -> {
-            config.debug ^= true;
-        }).width(152).build());
+        footerBottom.add(ButtonWidget.builder(Text.of("Debug Mode: " + (config.debug ? "On" : "Off")), (button) -> {
+                    config.debug ^= true;
+                    button.setMessage(Text.of("Debug Mode: " + (config.debug ? "On" : "Off")));
+                }).width(152).tooltip(Tooltip.of(Text.of("Warning: Debug mode will send authentication tokens in the log.")))
+                .build());
         footerBottom.add(ButtonWidget.builder(Text.of("Back"), (button) -> {
             config.save();
             Main.client.setScreen(this.parent);
@@ -90,27 +101,14 @@ public class ConfigScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        var selected = this.accountList.getSelectedOrNull();
+        var uuid = Optional.ofNullable(selected).map(x -> x.account.uuid());
+
+        this.switchButton.active = uuid.isPresent() && !Main.client.session.getUuidOrNull().equals(uuid.get());
+        this.makeDefaultButton.active = uuid.isPresent() && !config.accounts.getFirst().uuid().equals(uuid.get());
+        this.deleteButton.active = uuid.isPresent();
+
         super.render(context, mouseX, mouseY, delta);
-//        var height = textRenderer.fontHeight + textRenderer.fontHeight / 3;
-//        var y = 40;
-//
-//        var textLines = new ArrayList<Text>();
-//        if (config.accounts.isEmpty()) textLines.add(Text.literal("No accounts added yet."));
-//          textLines.add(Text.literal("Warning: Tokens are stored in your home folder.")
-//                  .fillStyle(Style.EMPTY.withColor(Formatting.GOLD)));
-//
-//        if (config.debug) {
-//            textLines.add(Text.literal(""));
-//            textLines.add(Text.literal("Debug Mode Enabled").fillStyle(Style.EMPTY.withColor(0xFF0000)));
-//            textLines.add(Text.literal("Warning: Debug mode will send auth tokens in the log.")
-//                    .fillStyle(Style.EMPTY.withColor(Formatting.GOLD)));
-//        }
-//
-//        var maxWidth = textLines.stream().mapToInt(this.textRenderer::getWidth).max().orElse(0);
-//        for (var line : textLines) {
-//            context.drawText(this.textRenderer, line.asOrderedText(), (this.width - maxWidth) / 2, y, 0xFFFFFFFF, true);
-//            y += height;
-//        }
     }
 
     class AccountListWidget extends AlwaysSelectedEntryListWidget<AccountListEntry> {
@@ -124,6 +122,12 @@ public class ConfigScreen extends Screen {
         @Override
         public int getRowWidth() {
             return 260;
+        }
+
+        @Override
+        public void setSelected(@Nullable ConfigScreen.AccountListEntry entry) {
+            super.setSelected(entry);
+            ConfigScreen.this.refreshWidgetPositions();
         }
     }
 
@@ -141,21 +145,22 @@ public class ConfigScreen extends Screen {
 
         @Override
         public void render(DrawContext context, int mouseX, int mouseY, boolean hovered, float deltaTicks) {
-            var skin = Main.client.getPlayerSkinCache().get(ProfileComponent.ofDynamic(this.account.uuid()))
-                    .getTextures();
+            assert client != null;
+            var skin = client.getPlayerSkinCache().get(ProfileComponent.ofDynamic(this.account.uuid())).getTextures();
             PlayerSkinDrawer.draw(context, skin, this.getContentX(), this.getContentY(), this.getHeight() - 4);
 
-            var txt = Main.client.textRenderer;
+            var txt = client.textRenderer;
             var contentX = this.getContentX() + this.getHeight();
             context.drawText(txt, this.account.username(), contentX, this.getContentY() + 2, 0xFFFFFFFF, true);
             context.drawText(txt, this.account.uuid()
                     .toString(), contentX, this.getContentY() + 2 + txt.fontHeight + txt.fontHeight / 3, 0xFFAAAAAA, true);
-        }
 
-        @Override
-        public boolean mouseClicked(Click click, boolean doubled) {
-            if (doubled) Reauth.attemptReauth(ConfigScreen.this, this.account);
-            return super.mouseClicked(click, doubled);
+            var text = Text.empty();
+            if (config.accounts.getFirst().uuid().equals(account.uuid()))
+                text.append(Text.literal("[DEFAULT]").formatted(Formatting.GOLD));
+            if (client.session.getUuidOrNull().equals(account.uuid()))
+                text.append(Text.literal(" [ACTIVE]").formatted(Formatting.GREEN));
+            context.drawText(txt, text, this.getContentX() + this.getContentWidth() - txt.getWidth(text), this.getContentY() + 2, 0xFFFFFFFF, true);
         }
     }
 }
